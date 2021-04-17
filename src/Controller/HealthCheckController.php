af7c13b15d0e5d5d0875cdaf2cfe89625c853eea
Andrés Montañez <andresmontanez@gmail.com>
2021-04-17 15:57:41 -0300

    Add health check

---
+++ src/Controller/HealthCheckController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HealthCheckController extends AbstractController
+{
+    /**
+     * @Route("/health-check", name="mage_health_check")
+     */
+    public function healthCheck(): Response
+    {
+        return new Response('Up', Response::HTTP_OK);
+    }
+}
